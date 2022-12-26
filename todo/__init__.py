c6669b5f1259d69c0aae9f1ed19b1d71ef94daeb
Jayalekshmi K S <jayalekshmiks112@gmail.com>
2022-12-26 11:33:50 +0530

    Initial commit

---
+++ todo/__init__.py
@@ -0,0 +1,13 @@
+__app_name__="todo"
+__version__="0.1.0"
+
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+    DB_READ_ERROR,
+    DB_WRITE_ERROR,
+    JSON_ERROR,
+    ID_ERROR,
+) = range(7)
+
